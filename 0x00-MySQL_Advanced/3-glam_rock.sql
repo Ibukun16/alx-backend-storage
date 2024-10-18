16586bdd56f10b83cf073092a58b0648e26fbd7a
Ibukun16 <ib.email>
2024-10-18 04:37:06 +0000

    Update tasks accordingly

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -4,7 +4,7 @@
 --Column names must be: band_name and lifespan
 -- (in years until 2022 - please use 2022 instead of YEAR(CURDATE()))
 -- use attributes formed and split for computing the lifespan
-SELECT band_name, IFNULL(split, '2022') - formed AS lifespan
-FROM metal_bands
-WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
+SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
+	FROM metal_bands
+	WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+	ORDER BY lifespan DESC;
