c0a7f8f06f4d5e0b150392c2a7b8d51626373b2a
Ibukun16 <ib.email>
2024-10-18 06:50:38 +0000

    Update tasks accordingly

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -3,7 +3,7 @@
 -- Import this table dump: metal_bands.sql.zip
 -- Column names must be: band_name and lifespan
 -- use attributes formed and split for computing the lifespan
-SELECT band_name, COALESCEL(split, 2022) - formed + 1 AS lifespan
+SELECT band_name, COALESCEL(split, 2020) - formed  AS lifespan
 FROM metal_bands
 WHERE style = 'Glam rock' 
 ORDER BY lifespan DESC;
