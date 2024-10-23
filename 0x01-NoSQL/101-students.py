c26c677f583fa34edd969dded798918505cc859a
Ibukun16 <ib.agunbiade18@gmail.com>
2024-10-22 21:58:54 -0700

    Update task 13

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Write a Python function that returns all students sorted by average score:
+
+Prototype: def top_students(mongo_collection):
+mongo_collection will be the pymongo collection object
+The top must be ordered
+The average score must be part of each item returns with key = averageScore
+"""
+
+
+import pymongo
+
+def top_students(mongo_collection):
+    """Return all students sorted by average score"""
+    students = mongo_collection.aggregate
