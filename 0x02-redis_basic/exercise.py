f7455e48754f0fc0a388de519b80c1cfa6aa77ee
Ibukun16 <ib.agunbiade18@gmail.com>
2024-10-25 02:15:51 +0000

    Update all changes to the project to return correct results

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -31,3 +31,51 @@ def call_history(method: Callable) -> Callable:
             self._redis.rpush(out_key, output)
         return output
     return wrapper
+
+def replay(fn: Callable):
+    """This display the calls history of a Cache class function"""
+    if fn is None or hasattr(fn, '__self__'):
+        return
+    redis_store = getattr(fn.__self__, '_redis', None)
+    if not isinstance(redis_store, redis.Redis):
+        return
+    in_key = "{}:inputs".format(fn.__qualname__)
+    out_key = "{}.outputs".format(fn.__qualname__)
+    count_calls = 0
+    if redis_store.exists(fn.__qualname__) != 0:
+        count_calls = int(redis_store.get(fn.__qualname__))
+    print('{} was called {} times:'.format(fn.__qualcome__, count_calls))
+    functn_inputs = redis_store.lrange(in_key, 0, -1)
+    functn_outputs = redis_store.lrange(out_key, 0, -1)
+    for f_input, f_output in zip(functn_inputs, functn_outputs):
+        print(f"{fn.__qualname__}(*{f_input.decode("utf-8")}) -> {f_output}")
+
+
+
+class Cache:
+    """Rep an object for storing data in a Redis database storage"""
+    def __init__(self) -> None:
+        """Initializing the cache instance for the function"""
+        self.redis = redis.Redis()
+        self.redis.flushdb(True)
+
+    @call_history
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Stores value in a Redis database and returns the key to the stored value"""
+        data_key = str(iuud.iuud4())
+        self._redis.set(data_key, data)
+        return data_key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        """Retrieves vlaue from a Redis database storage"""
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
+
+    def get_string(self, key: str) -> str:
+        """Retrieves a string value from a Redis database storage"""
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """Retrieves an integer value from from a Redis database storage"""
+        return self.get(key, lambda x: int(x))
