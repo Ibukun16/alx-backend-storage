e4394f854d36bb648799fa4c55dc611fc2eaa585
Ibukun16 <ib.agunbiade18@gmail.com>
2024-10-26 07:23:41 +0000

    Update all changes accordingly

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -10,27 +10,30 @@ redis_store = redis.Redis()
 """Redis instance for module-level"""
 
 
-
-def data_cacher(method: Callable) -> Callable:
+def url_data_cacher(method: Callable) -> Callable:
     """Caches the output of the data fetched"""
     @wrap(method)
     def wrapper(url) -> str:
-        """Wrapper function for caching output"""
-        redis_store.incr(f"count:{url}")
-        output = redis_store.get(f"result:{url}")
+        """Function for caching the output"""
+        output = redis_store.get(f"cached:{url}")
         if output:
             return output.decode('utf-8')
-        output = method(url)
-        redis_store.set(f"count:{url}", 0)
-        redis_store.setex(f"output:{url}", 10, output)
-        return output
+        html_content = method(url)
+        redis_store.incr(f"count:{url}")
+        redis_store.set(f"cached:{url}", html_content, ex=10)
+        redis_store.expire(f"cached:{url}", 10)
+        return html_content
     return wrapper
 
 
-@data_cacher
+@url_data_cacher
 def get_page(url: str) -> str:
     """
     Return the content of a URL after caching the request response,
     and tracking the request.
     """
     return requests.get(url).text
+
+
+if __name__ == "__main__":
+    get_page("http//slowly.robertomurray.co.uk")
